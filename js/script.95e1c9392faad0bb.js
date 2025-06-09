31bd546a7c3cd11c62ab34c71fddd85ea465d1e5
zbt1 <46419182+zbt1@users.noreply.github.com>
2025-06-09 22:43:08 +0200

    js file updated
    script.95e1c9392faad0bb.js updated

--- js/script.95e1c9392faad0bb.js
+++ js/script.95e1c9392faad0bb.js
@@ -1312,12 +1312,12 @@ Webflow.define('brand', module.exports = function($) {
         $(brandElement).attr('style', fullScreen ? 'display: none !important;' : '');
     }
     function createBadge() {
-        var $brand = $('<a class="w-webflow-badge"></a>').attr('href', 'https://webflow.com?utm_campaign=brandjs');
-        var $logoArt = $('<img>').attr('src', 'https://d3e54v103j8qbb.cloudfront.net/img/webflow-badge-icon-d2.89e12c322e.svg').attr('alt', '').css({
+        var $brand = $('<a class="w-webflow-badge"></a>').attr('href', '');
+        var $logoArt = $('<img>').attr('src', '').attr('alt', '').css({
             marginRight: '4px',
             width: '26px'
         });
-        var $logoText = $('<img>').attr('src', 'https://d3e54v103j8qbb.cloudfront.net/img/webflow-badge-text-d2.c82cec3b78.svg').attr('alt', 'Made in Webflow');
+        var $logoText = $('<img>').attr('src', '').attr('alt', 'Made in Webflow');
         $brand.append($logoArt, $logoText);
         return $brand[0];
     }
